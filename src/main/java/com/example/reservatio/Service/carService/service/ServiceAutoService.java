97983dc82d030232f134917f87832feca342df32
mavjudbek <mavjudbeksokhibov@gmail.com>
2023-12-19 04:33:11 +0500

    continuation

--- src/main/java/com/example/reservatio/Service/carService/service/ServiceAutoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.reservatio.Service.carService.service;
-
-import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
-
-@Service
-@Transactional
-public class ServiceAutoService {
-}
--- src/main/java/com/example/reservatio/user/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.reservatio.user;
-
-public class UserController {
-}
